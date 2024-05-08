9fcf2657bcfc1d9a47bf0dcbe6e41a51bc58ac8e
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-05-08 23:22:12 +0530

    P2 commit

---
+++ Patttern2.java
@@ -0,0 +1,26 @@
+/*
+
+ Pattern like 
+ *
+ **
+ ***
+ ****
+ *****
+
+ */
+import java.util.*;
+public class Patttern2  {
+    public static void main(String x[]){
+        Scanner sc=new Scanner(System.in);
+        //Taking the input from the user
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        //Writting down the logic:
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
